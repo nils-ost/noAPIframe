19023d5815941c6e710a2df2470f76021df13a2f
Nils Ost <home@nijos.de>
2025-07-21 11:37:35 +0200

    moved current state of library from an indev project

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # noAPIframe
 Basic framework for creating REST backends and just focus on businesslogic
+
+## Development Environment Setup
+
+Personally I use the following steps to set up my dev-environment. Abstract the steps to your needs.
+
+```
+sudo apt update; sudo apt install -y python3 virtualenv direnv
+virtualenv -p /usr/bin/python3 venv
+venv/bin/pip install -r requirements.txt
+venv/bin/pre-commit install
+sed -nr '/direnv hook bash/!p;$aeval "\$(direnv hook bash)"' -i ~/.bashrc
+source ~/.bashrc
+echo -e "source venv/bin/activate\nunset PS1" > .envrc
+direnv allow
+```
---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+import subprocess
+from os import path
+
+
+name = 'noAPIframe'
+
+
+version = None
+try:
+    version = subprocess.check_output('git describe', shell=True).decode('UTF-8').strip()
+    version = version.strip().replace('v', '', 1).rsplit('-', 1)[0].replace('-', '.')
+except Exception:
+    version = '0.0.0'
+
+
+def read_text(file_name):
+    with open(path.join(path.abspath(path.dirname(__file__)), file_name)) as f:
+        return f.read()
+
+
+params = dict(
+    name=name,
+    version=version,
+    description='Basic framework for creating REST backends and just focus on businesslogic',
+    long_description=read_text('README.md'),
+    long_description_content_type='text/markdown',
+    author='Nils Ost',
+    author_email='home@nijos.de',
+    license=read_text('LICENSE'),
+    packages=[
+        'noapiframe',
+    ],
+    install_requires=[
+        'CherryPy>=18.10.0',
+        'cherrypy-cors>=1.7.0',
+        'pycryptodome>=3.23.0',
+        'pymongo>=4.11.1'
+    ],
+    python_requires='>=3.8',
+)
+
+
+__name__ == '__main__' and setuptools.setup(**params)
